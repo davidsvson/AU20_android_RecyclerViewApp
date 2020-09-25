b2d35953e728128b4a7870e91513549de700869f
David Svensson <davidsvson@gmail.com>
2020-09-25 10:50:55 +0200

    Remove student

--- app/src/main/java/com/example/recyclerviewapp/StudentsRecycleAdapter.kt
+++ app/src/main/java/com/example/recyclerviewapp/StudentsRecycleAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.recyclerviewapp
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -32,10 +34,32 @@ class StudentsRecycleAdapter(val context: Context, val students : List<Student> 
 
     }
 
+    fun removeStudent(position: Int) {
+        val dialogBuilder = AlertDialog.Builder(context)
+
+        dialogBuilder.setTitle("Romove Student?")
+            .setMessage("Do you want to remove ${DataManager.students[position].name}?")
+            .setPositiveButton("Remove", { dialog, id ->
+                DataManager.students.removeAt(position)
+                notifyDataSetChanged()
+            })
+            .setNegativeButton("Cancel", { dialog, which ->
+                dialog.cancel()
+            })
+
+        val alert = dialogBuilder.create()
+        alert.show()
+
+    }
+
+
+
+
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
             val textName = itemView.findViewById<TextView>(R.id.nameTextView)
             val textClassName = itemView.findViewById<TextView>(R.id.classTextView)
             val doneButton = itemView.findViewById<CheckBox>(R.id.checkBox)
+            val deleteButton = itemView.findViewById<ImageButton>(R.id.deleteButton)
             var studentPosition = 0
 
             init {
@@ -43,6 +67,10 @@ class StudentsRecycleAdapter(val context: Context, val students : List<Student> 
                     DataManager.students[studentPosition].done = doneButton.isChecked
                     Toast.makeText(context, "${DataManager.students[studentPosition].name} is done",
                         Toast.LENGTH_SHORT).show()
+                }
+
+                deleteButton.setOnClickListener {
+                    removeStudent(studentPosition)
                 }
 
             }
